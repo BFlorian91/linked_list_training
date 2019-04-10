b3252000ee9f2080e7fa1316bc3d37c1ae8f3d32
BFlorian91 <flbeaumo@student.42.fr>
2019-04-10 18:08:22 +0200

    add insertion sort list

--- test.c
+++ test.c
@@ -6,44 +6,35 @@
 /*   By: flbeaumo <flbeaumo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 17:31:29 by flbeaumo          #+#    #+#             */
-/*   Updated: 2019/04/09 19:58:24 by flbeaumo         ###   ########.fr       */
+/*   Updated: 2019/04/10 18:05:49 by flbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "test.h"
 
-
-static t_dir       *push_back(t_dir *lst, int value)
+void        push_back(t_dir **lst, int value)
 {
     t_dir *new_node;
     t_dir *pos;
 
+    if (!lst)
+        return ;
     if ((new_node = (t_dir*)malloc(sizeof(t_dir))) == NULL)
-        return (NULL);
+        return ;
     new_node->number = value;
     new_node->next = NULL;
-    pos = lst;
-    while (pos && pos->next)
+    if (!*lst)
+        *lst = new_node;
+    else
     {
-        pos = pos->next;
+        pos = *lst;
+        while (pos && pos->next)
+        {
+            pos = pos->next;
+        }
+        pos->next = new_node;
     }
-    pos->next = new_node;
-    return (lst);
-}
-
-static int    swap(t_dir *prev, t_dir *current, t_dir *next)
-{
-    t_dir   *tmp;
-
-    tmp = next;
-    if (current->number > next->number)
-    {
-        current->next = next->next;
-        next->next = current;
-        prev->next = tmp;
-        return (1);
-    }
-    return (0);
 }
 
 void        display(t_dir *lst)
@@ -55,66 +46,67 @@ void        display(t_dir *lst)
     }
 }
 
-t_dir       *sort(t_dir *lst)
+void        lst_insert(t_dir **lst, t_dir *node)
 {
-    t_dir   *prev;
     t_dir   *current;
-    t_dir   *next;
-    t_dir   *start;
-    int     sorted;
+    t_dir   *tmp;
 
-    sorted = 1;
-    start = lst->next;
-    prev = lst;
-    current = start;
-    next = current->next;
-    lst = lst->next;
-    int i = 0;
-    int j = 0;
-    while (sorted)
+    current = *lst;
+    if (!current)
+        *lst = node;
+    else if (current->number > node->number)
     {
-        sorted = 0;
-        while (next)
-        {
-            printf("current->number: %d > next->number: %d ? \n", current->number, next->number);
-            ++i;
-            if (swap(prev, current, next))
-            {
-                sorted = 1;
-                prev = next;
-                next = next->next;
-            }
-            else
-            {
-                printf("Le else\n");                /*    <----- DEBUG -------|    */
-                prev = prev->next;
-                current = current->next;
-                next = next->next;
-            }
-        }
-        ++j;
-        printf("%d passage pour %d iterrations\n", j, i);
+        *lst = node;
+        node->next = current;
     }
-    return (start);
+    else
+    {
+        while (current->next && current->next->number <= node->number)
+            current = current->next;
+        tmp = current->next;
+        current->next = node;
+        node->next = tmp;
+    }
+}
+
+void       sort(t_dir **lst)
+{
+    t_dir   *nlst;
+    t_dir   *current;
+    t_dir   *tmp;
+
+    nlst = NULL;
+    if (!lst)
+        return ;
+    current = *lst;
+    while (current)
+    {
+        tmp = current->next;
+        current->next = NULL;
+        lst_insert(&nlst, current);
+        current = tmp;
+    }
+    *lst = nlst;
 }
 
 int         main(void)
 {
     t_dir *lst;
 
-    if ((lst = (t_dir *)malloc(sizeof(t_dir))) == NULL)
-        return (0);
-    lst->next = NULL; 
-    lst = push_back(lst, 38);
-    lst = push_back(lst, 18);
-    lst = push_back(lst, 2);
-    lst = push_back(lst, 34);
+    push_back(&lst, 38);
+    push_back(&lst, 18);
+    push_back(&lst, 2);
+    push_back(&lst, 34);
+    push_back(&lst, 138);
+    push_back(&lst, 8);
+    push_back(&lst, 2);
+    push_back(&lst, 0);
 
-    display(lst->next);
+    display(lst);
     printf("\n");
-    sort(lst);
+    sort(&lst);
     printf("\n");
-    display(lst->next);
+    display(lst);
     printf("\n");
 
     return (0);
